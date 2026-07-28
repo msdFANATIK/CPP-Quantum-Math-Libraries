333e44b88210718dd76eb0fb87067a3d705630af
msdFANATIK <msd.fanatik@gmail.com>
2026-07-28 18:20:48 +0300

    Clean up qm.hpp by removing blank line
    Removed an extra blank line to clean up the code.

---
+++ include/qm.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "types.hpp"
+#include "utils.hpp"
+#include "gates.hpp"
+#include "state.hpp"
+#include "measure.hpp"
+
+namespace qm {
+
+using QubitGate  = Matrix2x2;
+using TwoQubitGate = Matrix4x4;
+
+} // namespace qm
