9f9ba4e9b02cba7d252c15860bbd21d2b389ec7f
msdFANATIK <msd.fanatik@gmail.com>
2026-07-28 18:18:37 +0300

    Add constants for mathematical values in constants.hpp

---
+++ include/constants.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace qm {
+constexpr double PI        = 3.14159265358979323846;
+constexpr double INV_SQRT2 = 0.70710678118654752440;   // 1/√2
+constexpr double SQRT2     = 1.41421356237309504880;
+}
